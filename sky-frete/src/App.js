7e1afc0eb29175d24d82ad72242a8980aeabb5ec
Dudu Tavares <81688169+dudutavares1@users.noreply.github.com>
2021-12-12 23:35:14 -0300

    comecando projeto

---
+++ sky-frete/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Menu from './componentes/menu/menu'
+
+function App() {
+  return (
+    <div className="App">
+      <Menu/>
+     
+    </div>
+  );
+}
+
+export default App;
